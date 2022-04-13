aac02965166fc7ff58792a1bb6970e76aa6dd2a0
koltrastH <elina.hulbert@hyperisland.se>
2022-04-13 11:59:52 +0200

    configure mongodb and add requestLogger

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
-//API METHODS GO IN HERE
+//Import dependencies
 import express from "express";
 import mongodb from "mongodb";
 import cors from "cors";
+
+//Configure MongoDB
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017";
+const mongoClient = new mongodb.MongoClient(MONGODB_URL);
+mongoClient.connect();
+const db = mongoClient.db("ecommerce-group");
