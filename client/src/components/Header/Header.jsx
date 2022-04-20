7ffdbcfbb5ad99bca3392b2c1ee94239f6c3a2bc
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-04-20 14:16:51 +0200

    updated color scheme header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,20 +1,21 @@
 const Header = () => {
   return <div>
-  <div className=" flex w-screen h-12 bg-black justify-between items-center ">
-    <h2 className=" text-xl text-white pl-1 ">
-      logo
-    </h2>
-    <div className=" flex ">
-    <h3 className=" text-base text-white ">
-      cart
-    </h3>
-    <h3 className=" text-base text-white px-2 ">
-      menu
-    </h3>
 
+    <div className=" flex w-screen h-12 bg-white justify-between items-center ">
+      <div className=" flex ">
+        <h2 className=" text-xl text-black pl-1 ">
+          logo
+        </h2>
+      </div>
+      <div className=" flex ">
+        <h3 className=" text-base text-black ">
+          cart
+        </h3>
+        <h3 className=" text-base text-black px-2 ">
+          menu
+        </h3>
+      </div> 
     </div>
-    
-  </div>
   </div>
 };
 
