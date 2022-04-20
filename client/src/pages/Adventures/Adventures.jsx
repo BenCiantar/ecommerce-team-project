e0c91ce8608c5c49db76df1bccb1c46a9b214dc7
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-04-20 16:17:25 +0200

    Cleaned up some js syntax

--- client/src/pages/Adventures/Adventures.jsx
+++ client/src/pages/Adventures/Adventures.jsx
@@ -3,16 +3,17 @@ import { useEffect, useState } from "react";
 import { renderAllCategoryItems } from "../../scripts/tools";
 
 const Adventures = () => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
   
   useEffect(() => {
-   rows = getItemsFromDb("Adventure", setItems)
-   console.log(rows)
+   rows = getItemsFromDb("Adventure", setItems);
   }, []);
 
-  let rows = renderAllCategoryItems(items)
-  return <div>
-    {rows}
-  </div>;
+  let rows = renderAllCategoryItems(items);
+  return (
+    <div>
+      {rows}
+    </div>;
+   );
 };
 export default Adventures;
