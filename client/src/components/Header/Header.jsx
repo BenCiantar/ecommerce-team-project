81abf9c6b6c3ed8c865b3f8283e4c93ef6674c68
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-04-20 14:35:28 +0200

    added shopping cart png

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,14 @@
 const Header = () => {
   return <div>
 
-    <div className=" flex w-screen h-12 bg-white justify-between items-center ">
+    <div className=" flex w-screen h-12 bg-white justify-between items-center border-b-2 border-black ">
       <div className=" flex ">
         <h2 className=" text-xl text-black pl-1 ">
           logo
         </h2>
       </div>
       <div className=" flex ">
-        <h3 className=" text-base text-black ">
-          cart
-        </h3>
+        <img className=" width-4 " src="./shopping-cart.png"></img>
         <h3 className=" text-base text-black px-2 ">
           menu
         </h3>
