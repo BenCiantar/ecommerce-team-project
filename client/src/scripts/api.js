4b8222ab7f29e5db5a276b220ff952edb8fa7eb9
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-04-20 16:19:10 +0200

    Cleaned up some js syntax

--- client/src/scripts/api.js
+++ client/src/scripts/api.js
@@ -2,19 +2,17 @@ import { API_BASE_URL } from "../config"
 
 export function getItemsFromDb(category, setItems) {
     fetch (`${API_BASE_URL}/items/${category}`, {
-
         headers: {
             "content-type": "application/json"
         }
     })
     .then((response) => {   
-        return response.json()
+        return response.json();
     })
     .then((result) => {
-        setItems(result)
-
+        setItems(result);
     })
     .catch((err) => {
-        console.error(err)
-    })
+        console.error(err);
+    });
 }
