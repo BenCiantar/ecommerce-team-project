d1cf8036243da23c46b489b81a47f94bd9186110
koltrastH <elina.hulbert@hyperisland.se>
2022-04-20 16:24:07 +0200

    added a function to open the cart onClick

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
+import React from "react";
 import { Header } from "./components/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Adventures, Culture, BadURL404 } from "./pages/index";
+import { Cart } from "./components/index";
+import AppContext from "./scripts/context";
 
 function App() {
+  const [cartOpened, setCartOpened] = React.useState(false);
   return (
-    <div>
-      <Header/>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/adventures" element={<Adventures />} />
-          <Route path="/culture" element={<Culture />} />
-          <Route path="*" element={<BadURL404 />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <AppContext.Provider value={{ setCartOpened }}>
+      <div>
+        <Header onClickCart={() => setCartOpened(true)} />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={<div> {cartOpened ? <Cart /> : <Home />}</div>}
+            />
+            <Route path="/adventures" element={<Adventures />} />
+            <Route path="/culture" element={<Culture />} />
+            <Route path="*" element={<BadURL404 />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AppContext.Provider>
   );
 }
 
