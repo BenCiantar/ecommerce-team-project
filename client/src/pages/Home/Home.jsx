7fd3fa19e1f9f40fb9a70e0b9f7f334e380f4ab5
Felix Odenhall <90831955+felix-odenhall@users.noreply.github.com>
2022-04-20 14:05:22 +0200

    Fixed typo in img alt text

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
           <img
             className="object-cover w-full h-64"
             src="https://images.unsplash.com/photo-1550524514-efb9046e0fec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=653&q=80"
-            alt="ballet dancer on a street"
+            alt="Ballet dancer on a street"
           />
           <div className="absolute">
             <h4 className="mb-3 text-xl font-semibold tracking-tight text-white">
