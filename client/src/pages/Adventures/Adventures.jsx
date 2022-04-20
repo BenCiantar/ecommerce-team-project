2c6ea0f171138b054ea84fe173066192d650aeea
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-04-20 11:35:56 +0200

    calling the getItemFromDb function in the adventures page

--- client/src/pages/Adventures/Adventures.jsx
+++ client/src/pages/Adventures/Adventures.jsx
@@ -1,5 +1,12 @@
+import { getItemsFromDb } from "../../scripts/api";
+import { useEffect } from "react";
+
 const Adventures = () => {
+
+  useEffect(() => {
+    getItemsFromDb("Advetures")
+  }, []);
+
   return <div>Adventures</div>;
 };
-
 export default Adventures;
