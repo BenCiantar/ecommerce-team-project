10219fdb871e50b61d77a9188245da73abf445a3
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-04-20 11:17:31 +0200

    created a fetch function that fetches items from category in db

---
+++ client/src/scripts/api.js
@@ -0,0 +1,24 @@
+//function calls get method
+
+// fetch
+//put the result into an array
+//call a function that renders the items on the page
+import { API_BASE_URL } from "../../config"
+
+function getItemsFromDb(category) {
+    fetch (`${API_BASE_URL}${category}`, {
+
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+}
