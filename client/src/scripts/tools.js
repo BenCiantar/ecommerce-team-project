f82fed0547fed8563a7ceda426703a372048af68
koltrastH <elina.hulbert@hyperisland.se>
2022-04-22 14:47:14 +0200

    addItemToCart is now working in cart to increase quantity

--- client/src/scripts/tools.js
+++ client/src/scripts/tools.js
@@ -1,52 +1,58 @@
 import { FaShoppingCart, FaDollarSign } from "react-icons/fa";
 
 export function renderAllCategoryItems(items, cartItems, setCartItems) {
-    let rows = [];
-    items.forEach(item => {
-        rows.push(
-        <div>
-            <h1>{item.name}</h1>
-            <p><FaDollarSign />{item.price}</p>
-            <p>{item.description}</p>
-            <button className="buy-btn btn-in-stock" onClick={() => addItemToCart(item, cartItems, setCartItems)}>
-                <FaShoppingCart /> {item.price} kr
-            </button>
-        </div>
-        );
+  let rows = [];
+  items.forEach((item) => {
+    rows.push(
+      <div>
+        <h1>{item.name}</h1>
+        <p>
+          <FaDollarSign />
+          {item.price}
+        </p>
+        <p>{item.description}</p>
+        <button
+          className="buy-btn btn-in-stock"
+          onClick={() => addItemToCart(item, cartItems, setCartItems)}
+        >
+          <FaShoppingCart /> {item.price} kr
+        </button>
+      </div>
+    );
+  });
+  return rows;
+}
+
+export function addItemToCart(item, cartItems, setCartItems) {
+  const newArray = [...cartItems];
+  let itemExists = false;
+
+  for (let product of newArray) {
+    if (product.name === item.name) {
+      itemExists = true;
+      product.quantity += 1;
+    }
+    console.log(item, "exist", product.quantity);
+  }
+
+  if (!itemExists) {
+    newArray.push({
+      image: item.image,
+      name: item.name,
+      quantity: 1,
+      price: item.price,
     });
-    return rows;
+    console.log(item, "does not exist");
+  }
+
+  setCartItems(newArray);
+  console.log(cartItems);
 }
 
-function addItemToCart(item, cartItems, setCartItems) { 
-    const newArray = [...cartItems];
-    let itemExists = false;
-
-    for (let product of newArray) {
-        if (product.name === item.name){
-            itemExists = true;
-            product.quantity += 1;
-        }
-        console.log(item, "exist", product.quantity)
-    }
-    
-    if (!itemExists) {
-        newArray.push({
-            "image": item.image,
-            "name": item.name,
-            "quantity": 1,
-            "price": item.price
-        })
-        console.log(item, "does not exist")
-    }
-
-    setCartItems(newArray);
-    console.log(cartItems);
-}
-
-export function placeOrder(totalPrice){
-    alert(`Order placed! Total price: ${totalPrice} kr`);
+export function placeOrder(totalPrice) {
+  alert(`Order placed! Total price: ${totalPrice} kr`);
 }
 
 export function toggleHidden(target) {
-    document.getElementById(target).classList.toggle("hidden");
+  document.getElementById(target).classList.toggle("hidden");
 }
