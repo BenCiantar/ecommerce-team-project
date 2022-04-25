46d77d07684124621e545cbd93a7d792b8477b39
felix-odenhall <90831955+felix-odenhall@users.noreply.github.com>
2022-04-25 14:35:34 +0200

    added POST metod to users collection, posted a user to the collection

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <form>
+        <h1>Sign In</h1>
+        <label>
+          Email:
+          <input
+            className="border-solid border-2 border-slate-600"
+            type="text"
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            className="border-solid border-2 border-slate-600"
+            type="text"
+            required
+          />
+        </label>
+        <button>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
