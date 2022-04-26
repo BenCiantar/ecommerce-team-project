acb108c54331580d6c8df0404d6999e8fc03d74d
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-04-26 15:22:08 +0200

    added props to culture page for sort and cart

--- client/src/pages/Culture/Culture.jsx
+++ client/src/pages/Culture/Culture.jsx
@@ -4,17 +4,18 @@ import { renderAllCategoryItems } from "../../scripts/tools";
 import { Sort } from "../../components";
 
 
-const Culture = () => {
+const Culture = ({ cartItems, setCartItems }) => {
   const [items, setItems] = useState([]);
   
   useEffect(() => {
     getItemsFromDb("Culture", setItems);
   }, []);
 
-  let rows = renderAllCategoryItems(items);
+  let rows = renderAllCategoryItems(items, cartItems, setCartItems);
   
   return (
     <div>
+      <Sort items={items} setItems={setItems} />
       {rows}
     </div>
    );
