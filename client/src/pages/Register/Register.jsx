d6689eaca2b435967790b78f3dfc9509cf65133d
koltrastH <elina.hulbert@hyperisland.se>
2022-04-26 11:24:47 +0200

    registering a user in database users collection onsubmit

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import axios from "axios";
 
 const Register = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let obj = {
+      _id: e.target[0].value,
+      password: e.target[1].value,
+    };
+    axios.post("http://localhost:8080/users", obj);
+
+    //     method: 'POST',
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ // We should keep the fields consistent for managing this data later
+    //         email: name,
+    //         clockedIn:false,
+    //         dates:[]
+    //     })
+    // })
+    // .then(()=>{
+    //     // Once posted, the user will be notified
+    //     alert('You have been added to the system!');
+    // })
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Sign In</h1>
-        <label for="email">
+        <label htmlFor="email">
           Email:
           <input
             className="border-solid border-2 border-slate-600"
@@ -15,7 +41,7 @@ const Register = () => {
             placeholder="Your email"
           />
         </label>
-        <label for="password">
+        <label htmlFor="password">
           Password:
           <input
             className="border-solid border-2 border-slate-600"
@@ -25,7 +51,7 @@ const Register = () => {
             placeholder="Your password"
           />
         </label>
-        <button>Register</button>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
