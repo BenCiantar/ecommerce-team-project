0b6adcc05df0aad3adb2ea5ff9b0a3d51fe6771b
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2022-04-26 10:29:47 +0200

    added Sort component

---
+++ client/src/components/Sort/Sort.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { AiOutlineShoppingCart, AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+
+const Sort = () => {
+
+    
+    
+
+    return (
+        <p>sort</p>
+    )
+
+};
+
+
+
+export default Sort;
