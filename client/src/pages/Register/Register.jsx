e2e27e5889ddca987d0a8608e2ae097c4d2d519b
koltrastH <elina.hulbert@hyperisland.se>
2022-04-26 10:30:45 +0200

    added placeholder and id to input and for attributes to label in register page

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
   return (
     <div>
       <form>
         <h1>Sign In</h1>
-        <label>
+        <label for="email">
           Email:
           <input
             className="border-solid border-2 border-slate-600"
-            type="text"
+            type="email"
             required
+            id="email"
+            placeholder="Your email"
           />
         </label>
-        <label>
+        <label for="password">
           Password:
           <input
             className="border-solid border-2 border-slate-600"
-            type="text"
+            type="pasword"
             required
+            id="password"
+            placeholder="Your password"
           />
         </label>
         <button>Register</button>
