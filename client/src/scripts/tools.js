986537c8f1f62f449a55ef516f230408177399be
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-04-27 11:58:55 +0200

    Final styling for item list items on Adventure page

--- client/src/scripts/tools.js
+++ client/src/scripts/tools.js
@@ -4,18 +4,30 @@ export function renderAllCategoryItems(items, cartItems, setCartItems) {
   let rows = [];
   items.forEach((item) => {
     rows.push(
-      <div className="w-full h-96 p-3 flex flex-col justify-start items-center rounded-md">
-        <img src={item.image} alt={item.alt} className="w-full h-1/2 object-cover mb-2 rounded-md" />
+      <div className="w-full h-96 p-3 bg-white border-2 flex flex-col justify-start items-center">
+        <div className="w-full h-1/2 pb-2">
+          <img src={item.image} alt={item.alt} className="w-full h-full object-cover" />
+        </div>
+        <div className="w-full h-1/3">
         <div className="w-full flex flex-col justify-start items-left">
           <h1 className="text-lg mb-1 font-bold">{item.name}</h1>
-          <p className="mb-3">{item.company}</p>
+          <p className="text-sm pb-3">{item.company}</p>
+          <p className="text-md pb-4">{item.shortdes}</p>
         </div>
-        <button
-            className="bg-green-500 text-white w-1/4 min-w-fit flex flex-row justify-center items-center p-1 rounded-md shadow-md"
+        <div className="w-full flex flex-row justify-between items-center">
+          <button
+            className="bg-green-600 text-white mr-1 w-1/2 min-w-fit flex flex-row justify-center items-center p-1 shadow-md"
+          >
+            Read More
+          </button>
+          <button
+            className="bg-green-600 text-white ml-1 w-1/2 min-w-fit flex flex-row justify-center items-center p-1 shadow-md"
             onClick={() => addItemToCart(item, cartItems, setCartItems)}
           >
             <FaShoppingCart /> <p className="ml-2">{item.price} kr</p>
           </button>
+          </div>
+        </div>
       </div>
     );
   });
