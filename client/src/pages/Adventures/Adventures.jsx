d250a6f8228aa3315079a0181eedc087fdd27940
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-04-27 11:09:55 +0200

    Added background color and min height to item list background

--- client/src/pages/Adventures/Adventures.jsx
+++ client/src/pages/Adventures/Adventures.jsx
@@ -11,7 +11,7 @@ const Adventures = ({ cartItems, setCartItems }) => {
 
   let rows = renderAllCategoryItems(items, cartItems, setCartItems);
   return (
-    <div className="bg-background grid grid-cols-1 gap-5 p-5 md:grid-cols-3 lg:grid-cols-4">
+    <div className="bg-background min-h-screen grid grid-cols-1 gap-5 p-5 md:grid-cols-3 lg:grid-cols-4">
       {rows}
     </div>
   );
