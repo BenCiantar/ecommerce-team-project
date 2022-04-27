bc7197a8088fce699a8a7ad9f8b3b86ab332387d
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-04-27 15:49:13 +0200

    Added needed parameters on placeOrder

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -47,7 +47,7 @@ const Cart = ({ cartItems, setCartItems }) => {
           <div className="">Total: {totalPrice} kr</div>
           <button
             className="bg-green-500 text-white p-2 rounded-md shadow-md"
-            onClick={() => placeOrder(totalPrice)}
+            onClick={() => placeOrder(cartItems, setCartItems, totalPrice)}
           >
             Place Order
           </button>
