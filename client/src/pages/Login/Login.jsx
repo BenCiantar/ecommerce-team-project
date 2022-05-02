4e5f215cc2c029220b8589e657c608332862b27b
felix-odenhall <90831955+felix-odenhall@users.noreply.github.com>
2022-05-02 14:39:54 +0200

    added style for the login page, added box-shadow-full to tailwind.condig.js

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -29,33 +29,45 @@ const Login = ({ currentUser, setCurrentUser }) => {
       });
   }
   return (
-    <div>
-      <form onSubmit={loginUser}>
-        <h1>Log in</h1>
-        <label htmlFor="email">
-          Email:
-          <input
-            className="border-solid border-2 border-slate-600"
-            type="email"
-            required
-            id="email"
-            placeholder="Enter your email"
-          />
-        </label>
-        <label htmlFor="password">
-          Password:
-          <input
-            className="border-solid border-2 border-slate-600"
-            type="password"
-            required
-            id="password"
-            placeholder="Enter a password"
-          />
-        </label>
-        <button type="submit">LOG IN</button>
-        <Link to="/register">I don't have an account</Link>
-      </form>
-    </div>
+    <main className="flex justify-center items-center my-14 ">
+      <section className="flex flex-col shadow-full w-4/5 sm:w-3/4 md:w-1/2 lg:w-1/3 h-4/5 justify-center items-center py-8">
+        <h1 className="  text-2xl font-semibold uppercase my-6">Log in</h1>
+        <form
+          onSubmit={loginUser}
+          className="flex flex-col justify-center items-center"
+        >
+          <label htmlFor="email">
+            <h3 className=" text-sm uppercase mb-1">E-mail:</h3>
+            <input
+              className="border-solid border border-slate-400 w-64 px-2 py-1 mb-4"
+              type="email"
+              required
+              id="email"
+              placeholder="Enter your email"
+            />
+          </label>
+          <label htmlFor="password">
+            <h3 className=" text-sm uppercase mb-1">Password:</h3>
+            <input
+              className="border-solid border border-slate-400 w-64 px-2 py-1 mb-6"
+              type="password"
+              required
+              id="password"
+              placeholder="Enter a password"
+            />
+          </label>
+          <button
+            className="bg-green-600 md:hover:bg-green-400 text-white w-1/2 min-w-fit flex flex-row justify-center items-center p-1 shadow-md mb-2 "
+            type="submit"
+          >
+            LOG IN
+          </button>
+          <Link to="/register" className="text-xs text-slate-400">
+            I don't have an account
+          </Link>
+        </form>
+      </section>
+    </main>
   );
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        full: "0 0 30px -12px rgba(0, 0, 0, 0.3)",
+      },
       colors: {
-        'background': '#f2f2f2',
+        background: "#f2f2f2",
       },
     },
   },
   plugins: [],
-}
+};
