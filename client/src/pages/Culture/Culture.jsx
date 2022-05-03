85aa23c4e9ef5f6d6450d3317b5ad89069888b68
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-05-03 14:29:30 +0200

    Deleted old version comment

--- client/src/pages/Culture/Culture.jsx
+++ client/src/pages/Culture/Culture.jsx
@@ -1,28 +1,3 @@
-// import { getItemsFromDb } from "../../scripts/api";
-// import { useEffect, useState } from "react";
-// import { renderAllCategoryItems } from "../../scripts/tools";
-// import { Sort } from "../../components";
-
-// const Culture = ({ cartItems, setCartItems }) => {
-//   const [items, setItems] = useState([]);
-
-//   useEffect(() => {
-//     getItemsFromDb("Culture", setItems);
-//   }, []);
-
-//   let rows = renderAllCategoryItems(items, cartItems, setCartItems);
-  
-//   return (   
-//     <div className="bg-slate-500 ">
-//       <Sort items={items} setItems={setItems} />
-//       <div className=" grid grid-cols-1 gap-5 p-5 md:grid-cols-2 lg:grid-cols-3 ">
-//         {rows}
-//       </div>
-//     </div>
-//    );
-// };
-// export default Culture;
-
 import { getItemsFromDb } from "../../scripts/api";
 import { useEffect, useState } from "react";
 import { renderAllCategoryItems } from "../../scripts/tools";
